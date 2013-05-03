7698f2f401ab9d846c883a0d0b4eb8cda6f49fe2
Chris Price <chris@puppetlabs.com>
2013-05-02 18:14:26 -0700

    Fix broken license in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,8 @@
-(defproject freemarket/freemarket "0.1"
-   :description "yo"
+(defproject freemarket/freemarket "0.1.1"
+   :description "A clojure Producer/Consumer library"
+   :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"
+            :distribution :repo}
+   :url "https://github.com/cprice-puppet/freemarket"
    :dependencies [[org.clojure/clojure "1.4.0"]
                   [org.clojure/tools.logging "0.2.3"]])
